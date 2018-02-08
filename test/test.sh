6b60705d6733c018af31d977d7d7ee97292b8eca
Chingis Sandanov <chingis@wodby.com>
2018-02-08 17:36:06 +0700

    Run init action manually in tests

--- test/test.sh
+++ test/test.sh
@@ -7,6 +7,8 @@ if [[ -n "${DEBUG}" ]]; then
 fi
 
 docker-compose up -d
-docker-compose exec cachet make check-ready delay_seconds=5 max_try=30 -f /usr/local/bin/actions.mk
+docker-compose exec cachet make check-ready max_try=10 -f /usr/local/bin/actions.mk
+docker-compose exec nginx make check-ready max_try=10 -f /usr/local/bin/actions.mk
+docker-compose exec cachet make init -f /usr/local/bin/actions.mk
 docker-compose exec cachet curl -Ls nginx | grep -q "Setup Cachet"
 docker-compose down
