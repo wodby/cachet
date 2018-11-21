958fe531942d214cac6229ace92a07ac3cc3f323
Chingis Sandanov <chingis@wodby.com>
2018-11-21 20:37:16 +0700

    Update tests

--- test/test.sh
+++ test/test.sh
@@ -9,6 +9,6 @@ fi
 docker-compose up -d
 docker-compose exec cachet make check-ready max_try=10 -f /usr/local/bin/actions.mk
 docker-compose exec nginx make check-ready max_try=10 -f /usr/local/bin/actions.mk
-docker-compose exec -e DEBUG cachet make init -f /usr/local/bin/actions.mk
+docker-compose exec cachet make init -f /usr/local/bin/actions.mk
 docker-compose exec cachet curl -Ls nginx | grep -q "Setup Cachet"
 docker-compose down
